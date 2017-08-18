97c9091b2b7cf6320fff8d2597d1f7e9c2ee7fd0
Dan Nollette <nollette.dan@gmail.com>
2017-08-18 09:25:33 -0700

    Add backend function pingPongReplacer, and return an alert box and no output on the page itself, if input is not a number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 //User Interface (UI or Front-End)
 $(document).ready(function(){
   $("#ping-pong-form").submit(function(event){
-    var numberInput = $("#number-input").val();
-    var output = numberInput;
+    var numberInput = parseFloat($("#number-input").val());
+    var output = pingPongReplacer(numberInput);
     $("#output").text(output);
     event.preventDefault();
   });
 });
+
+function pingPongReplacer(number){
+  if (number !== number){
+    alert ("That is not a valid input. Please enter a whole number greater than one.");
+    return "";
+  }
+};
